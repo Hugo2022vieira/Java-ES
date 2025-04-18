1dd7f76cb9ab68a908ac1d2c6bc8a32a642da7f5
Hugo Vieira <108480477+Hugo2022vieira@users.noreply.github.com>
2025-04-18 14:32:32 -0300

    Create AlgoritmosOrdenacao.java

---
+++ AlgoritmosOrdenacao.java
@@ -0,0 +1,141 @@
+// AlgoritmosOrdenacao.java
+
+import java.util.Arrays;
+
+public class SortingAlgorithms {
+
+    public static void bubbleSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int key = arr[i];
+            int j = i - 1;
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = key;
+        }
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            int temp = arr[minIdx];
+            arr[minIdx] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) return;
+        int mid = arr.length / 2;
+        int[] left = Arrays.copyOfRange(arr, 0, mid);
+        int[] right = Arrays.copyOfRange(arr, mid, arr.length);
+
+        mergeSort(left);
+        mergeSort(right);
+        merge(arr, left, right);
+    }
+
+    private static void merge(int[] arr, int[] left, int[] right) {
+        int i = 0, j = 0, k = 0;
+        while (i < left.length && j < right.length) {
+            arr[k++] = (left[i] < right[j]) ? left[i++] : right[j++];
+        }
+        while (i < left.length) arr[k++] = left[i++];
+        while (j < right.length) arr[k++] = right[j++];
+    }
+
+    public static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        return i + 1;
+    }
+
+    public static void heapSort(int[] arr) {
+        int n = arr.length;
+        for (int i = n / 2 - 1; i >= 0; i--) heapify(arr, n, i);
+        for (int i = n - 1; i > 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    private static void heapify(int[] arr, int n, int i) {
+        int largest = i, l = 2 * i + 1, r = 2 * i + 2;
+        if (l < n && arr[l] > arr[largest]) largest = l;
+        if (r < n && arr[r] > arr[largest]) largest = r;
+        if (largest != i) {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] array = {64, 25, 12, 22, 11};
+        System.out.println("Original: " + Arrays.toString(array));
+
+        int[] bubble = array.clone();
+        bubbleSort(bubble);
+        System.out.println("Bubble Sort: " + Arrays.toString(bubble));
+
+        int[] insertion = array.clone();
+        insertionSort(insertion);
+        System.out.println("Insertion Sort: " + Arrays.toString(insertion));
+
+        int[] selection = array.clone();
+        selectionSort(selection);
+        System.out.println("Selection Sort: " + Arrays.toString(selection));
+
+        int[] merge = array.clone();
+        mergeSort(merge);
+        System.out.println("Merge Sort: " + Arrays.toString(merge));
+
+        int[] quick = array.clone();
+        quickSort(quick, 0, quick.length - 1);
+        System.out.println("Quick Sort: " + Arrays.toString(quick));
+
+        int[] heap = array.clone();
+        heapSort(heap);
+        System.out.println("Heap Sort: " + Arrays.toString(heap));
+    }
+}
